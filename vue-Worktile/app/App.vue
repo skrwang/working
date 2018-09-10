5eb2ac6a8feeeef1db11642ffc39ef8324b77c77
skrwang <you@example.com>
2018-09-10 19:07:04 +0800

    clear node_modules

--- vue-Worktile/app/App.vue
+++ vue-Worktile/app/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div>
     <header>
-        <ul>
-            <!-- cur 样式要在点击的标题与路由中的挑剔匹配时展示 -->
-            <li v-for="item of tabNav" :class="{cur:item.title == $route.name}">
-                <!-- 做跳转 -->
-            <router-link :to='item.url'>{{item.title}}</router-link>
-            </li>
-        </ul>
+        <div class="top-area">
+          <a href="javascript:;" class="logo">
+            <img src="https://s3.cn-north-1.amazonaws.com.cn/lclogo/team_logo_default.png" alt="">
+          </a>
+        </div>
+        <div class="middle-area">
+            <ul>
+                <!-- cur 样式要在点击的标题与路由中的挑剔匹配时展示 -->
+                <li v-for="item of tabNav" :class="{cur:item.title == $route.name}">
+                    <!-- 做跳转 -->
+                    <router-link :to='item.url'  class="app-item">
+                        
+                    </router-link>
+                </li>
+            </ul>
+        </div>
     </header>
     <!-- 用于展示路由连接的组件 -->
-    <router-view>
+    <!-- <router-view>
 
-    </router-view>
+    </router-view> -->
   </div>
 </template>
 
@@ -48,37 +57,77 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
     *{
       padding: 0;
       margin: 0;
+      box-sizing: border-box;
     }
-    header{
-      height: 50px;
-      border-bottom: 1px solid #eee;
-    }
-    header ul{
-        list-style:none;
-    }
-    header ul li {
-        float: left;
-        width: 80px;
-        height: 48.2px;
-        line-height: 40px;
-        text-align: center;
-    }
-    li.cur{
-        color: #22d7bb;
-        border-bottom: 2px solid #22d7bb;
+    img {
+      border: 0;
+      vertical-align: middle;
     }
     a{
-        display: block;
-        width: 100%;
-        height: 100%;
-        text-decoration: none;
-        color: black;
+      text-decoration: none;
     }
-    .router-link-active{
-        color: green;
+    header{
+      position: relative;
+      width: 70px;
+      background: #22d7bb;
+      overflow-y: hidden;
+    }
+    header .logo{
+      margin: 18px 0;
+      text-align: center;
+      display: block;
+      background-color: transparent;
+    }
+    header .logo img{
+      width: 50px;
+      height: 50px;
+      border-radius: 12px;
+    }
+    header .middle-area{
+      text-align: center;
+    }
+    .middle-area ul{
+      list-style:none;
+    }
+    .middle-area ul li {
+        cursor: pointer;
+        position: relative;
+    }
+    .middle-area ul li.cur a.app-item{
+        background: #18bfa4;
+    }
+    .app-item {
+        display: block;
+        text-decoration: none;
+        height: 70px;
+        transition: box-shadow .3s,-webkit-box-shadow .3s;
+    }
+    a.app-item:before {
+        content: "";
+        position: absolute;
+        right: 0;
+        top: 30px;
+        width: 0;
+        height: 0;
+        border-top: 6px solid transparent;
+        border-right: 5px solid #fdfdfd;
+        border-bottom: 6px solid transparent;
+    }
+    .item-icon {
+        margin-top: 15px;
+        display: inline-block;
+        color: #fff;
+        font-size: 24px;
+        opacity: .8;
+        -webkit-transition: opacity .5s,-webkit-transform .5s;
+        transition: opacity .5s,-webkit-transform .5s;
+        transition: transform .5s,opacity .5s;
+        transition: transform .5s,opacity .5s,-webkit-transform .5s;
+        opacity: 1;
+        transform: translateY(0);
     }
 </style>
